28703bdc1637e7390291ef6427a60e214c3a6598
Luana Reis <luanasr@outlook.com.br>
2018-11-22 18:07:25 -0300

    Primeiro código

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Luana Reis
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args){
+     
+        System.out.println("Olá galera!");
+        
+    }
+    
+    
+}
